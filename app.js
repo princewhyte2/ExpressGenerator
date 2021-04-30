600bdba7c90ce82db5ef1ec580e7cf92b4bca356
princewhyte2 <princewhyte02@gmail.com>
2021-04-30 22:02:00 +0100

    HTTPS

--- app.js
+++ app.js
@@ -1,46 +1,57 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const mongoose = require('mongoose');
-var config = require('./config');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const mongoose = require("mongoose");
+var config = require("./config");
 
-var session = require('express-session');
-
+var session = require("express-session");
 
 const url = config.mongoUrl;
 
 // var FileStore = require('session-file-store')(session);
-var passport = require('passport');
+var passport = require("passport");
 // var authenticate = require('./authenticate');
 // const Dishes = require('./models/dishes');
 
 const connect = mongoose.connect(url);
 
+connect.then(
+  (db) => {
+    console.log("Connected correctly to server");
+  },
+  (err) => {
+    console.log(err);
+  }
+);
 
-connect.then((db) => {
-  console.log("Connected correctly to server");
-}, (err) => { console.log(err); });
-
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var dishRouter = require('./routes/dishRouter');
-var promoRouter = require('./routes/promoRouter');
-var leaderRouter = require('./routes/leaderRouter');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var dishRouter = require("./routes/dishRouter");
+var promoRouter = require("./routes/promoRouter");
+var leaderRouter = require("./routes/leaderRouter");
 
 var app = express();
-
+app.all("*", (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+    res.redirect(
+      307,
+      "https://" + req.hostname + ":" + app.get("secPort") + req.url
+    );
+  }
+});
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 //app.use(cookieParser('12345-67890-09876-54321'));
 
@@ -53,14 +64,14 @@ app.use(passport.session());
 //   resave: false,
 //   store: new FileStore()
 // }));
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Secure traffic only
 
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
-
-app.use('/dishes', dishRouter);
-app.use('/promotions', promoRouter);
-app.use('/leaders', leaderRouter);
+app.use("/dishes", dishRouter);
+app.use("/promotions", promoRouter);
+app.use("/leaders", leaderRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -71,11 +82,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
